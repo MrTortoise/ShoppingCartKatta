ee426648aeb13524c6c9bc65dfe1781b19ef4497
john nicholas <themrtortoise@gmail.com>
2017-01-25 15:12:22 +0000

    kata 2 ... add item get total

---
+++ 2/ShoppingCart/ShoppingCart.Tests/Class1.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Tests
+{
+  
+    public class Class1
+    {
+        private const string Sku1 = "sku1";
+
+        [Test]
+        public void ScanItemGetItem()
+        {
+            var item = new Item(Sku1, 30);
+            var thing = new Thing();            
+            var basket = thing.ScanItem(item);
+            Assert.That(basket.Total, Is.EqualTo(item.Price));
+        }
+    }
+
+    internal class Thing
+    {
+        public Thing()
+        {
+        }
+
+        internal Basket ScanItem(Item item)
+        {
+            return new Tests.Basket(item);
+        }
+    }
+
+    internal class Basket
+    {
+        private Item item;
+
+        public Basket(Item item)
+        {
+            this.item = item;
+        }
+
+        internal int Total()
+        {
+            return item.Price;
+        }
+    }
+}
---
+++ 2/ShoppingCart/ShoppingCart.Tests/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingCart.Tests
+{
+    internal class Item
+    {
+        private string sku1;
+        private int price;
+
+        public Item(string sku1, int price)
+        {
+            this.sku1 = sku1;
+            this.price = price;
+        }
+
+        public int Price => price;
+    }
+}
