50e8d057994d9402fe26a8d4fc58fb9d1a498ffe
john nicholas <themrtortoise@gmail.com>
2017-01-25 22:22:24 +0000

    explanation of new scope

--- 5/ShoppingCart.Tests/ShoppingCart.Tests/Class1.cs
+++ 5/ShoppingCart.Tests/ShoppingCart.Tests/Class1.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace ShoppingCart.Tests
 {
+    /*
+     * Scan items into a basket.
+     * Get a total of an order.
+     * Add Promotion buy x get y of item z
+     * Add Promotion 10% discount on item z
+     * Have items on the order showing discount.
+     * 
+     */
     public class Class1
     {
     }
